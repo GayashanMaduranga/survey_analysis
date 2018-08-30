57bb048b9c5ee4cd476340d396c6c977461b95cf
gayashan kariyawasam <gmkariyawasam@gmail.com>
2018-08-30 12:49:43 +0530

    Renamed Field Names

--- Survey Plots.R
+++ Survey Plots.R
@@ -1,14 +1,18 @@
 data <- read.csv('Survey on career interest of undergraduate.csv',header = TRUE,stringsAsFactors = FALSE)
 head(data)
+library(dplyr)
+library(ggplot2)
 
-doubleValues <- function(x){
-  return (x*2)
-}
+new_data <- data %>% rename(University = What.is.your.University.) %>%
+            rename(DegreeProgramme = What.is.your.Studying.Degree.Programme.) %>%
+            rename(HigherStudies = Do.you.wish.to.do.higher.studies.) %>%
+            rename(WorkPlace = Place.interest.to.work) %>%
+            rename(Distance = How.far.should.you.hope.live.from.work.) %>%
+            rename(Salary = Expecting.salary.range.) %>%
+            rename(Sector = Wish.to.work.sector.) %>%
+            rename(Field = Wish.to.work.industry.field.) %>%
+            rename(job = Wish.to.work.industry.field.job.) %>%
+            rename(Position =In.a.Department.which..position.would.u.prefer.) %>%
+            select(Position) %>% head()
 
-v <- 1:10
-
-v <- sapply(v, doubleValues)
-v
-mtcars2 <- mtcars
-
-mtcars2$vs <- sapply(mtcars2$vs,doubleValues)
+colnames(data)
